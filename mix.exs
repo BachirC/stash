adb1004f43bcb1bc6deca03d233186dcbe4003bc
Bachir Çaoui <bachir@followanalytics.com>
2017-03-02 19:06:57 +0100

    Add elixir rubocop : Credo

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Stash.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:ex_machina, "~> 1.0", only: :test}
+      {:ex_machina, "~> 1.0", only: :test},
+      {:credo, "~> 0.6.1", only: [:dev, :test]}
    ]
   end
 
